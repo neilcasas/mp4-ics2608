f00a5542d4b6dbd573e5c2620b024b5c69a37f68
Neil Casas <neilalfonzcasas@gmail.com>
2024-12-05 15:51:01 +0800

    Created date object context listener (again)

--- src/classes/DateContextListener.java
+++ src/classes/DateContextListener.java
@@ -1,7 +1,14 @@
-import javax.servlet.ServletContextListener;
+import javax.servlet.*;
+import java.util.Date;
+
 
 public class DateContextListener implements ServletContextListener{
     public void contextInitialized(ServletContextEvent sce) {
+        ServletContext context = sce.getServletContext();
+        context.setAttribute("date", new Date());
+    }
+    
+    public void contextDestroyed(ServletContextEvent sce){
         
     }
 }
